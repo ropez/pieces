3fafb88c2e5a4acb9c9c1694a8738b315c262c0e
Robin Pedersen <robinpeder@gmail.com>
2007-02-05 15:12:22 +0000

    Cleaned up test function.

--- src/test.cpp
+++ src/test.cpp
@@ -7,36 +7,31 @@
 int main()
 {
     Piece t;
-
     t.set("robin", 10).set("angle", 2e23).set("documentation", "supercool stuff");
 
     List l;
-    l.add('a');
-    l.add(10);
-    l.add(2.34);
-    l.add("joakim simonsson");
+    l.add('a').add(10).add(2.34).add("joakim simonsson");
 
-    std::cout << l << std::endl;
-    std::stringstream s;
-    s << l;
-    List m;
-    s >> m;
-    std::cout << m << std::endl;
-    std::cout << m.get<double>(2) << std::endl;
+    std::cout << "A piece:\n" << t << std::endl;
+    std::cout << "A list:\n" << l << std::endl;
 
-
+    std::cout << "Reading from the piece:" << std::endl;
     std::cout << t.get<int>("robin") << std::endl;
     std::cout << t.get<double>("angle") << std::endl;
 
-    std::cout << t << std::endl;
-    std::stringstream ss;
-    ss << t;
     Piece p;
-    ss >> p;
-    p.set<Piece>("a-sub-piece", t);
-    p.set("a-sub-list", l);
-    std::cout << p << std::endl;
-    std::cout << p.get<String>("a-sub-piece") << std::endl;
-    std::cout << p.get<List>("a-sub-list").get<String>(3) << std::endl;
+    p.set("sub-piece", t);
+    p.set("sub-list", l);
+
+    std::cout << "A piece that contains a list and a piece:\n" << p << std::endl;
+
+    std::cout << "Extracted data:" << std::endl;
+    std::cout << p.get<Piece>("sub-piece").get<String>("documentation") << std::endl;
+    std::cout << p.get<List>("sub-list").get<String>(3) << std::endl;
+
+    List q;
+    q.add(t).add(l);
+
+    std::cout << "Can do the same with a list:\n" << q << std::endl;
 }
 
