16f9a47995fe30fcdebd6e8755dd7537289d53b2
Robin Pedersen <robinpeder@gmail.com>
2007-02-12 22:45:15 +0000

    Improved code quality a little bit, and implemented operators for all built-in types.

--- src/pieces/data_stream.cpp
+++ src/pieces/data_stream.cpp
@@ -1,6 +1,9 @@
 
 #include "data_stream.h"
 
+#include <algorithm>
+
+
 namespace Pieces
 {
 
@@ -22,13 +25,129 @@ ByteArray DataStream::data() const
 }
 
 
+DataStream& DataStream::operator<<(bool i)
+{
+    write(reinterpret_cast<char*>(&i), sizeof(bool));
+
+    return *this;
+}
+
+
+DataStream& DataStream::operator<<(char i)
+{
+    write(reinterpret_cast<char*>(&i), sizeof(char));
+
+    return *this;
+}
+
+
+DataStream& DataStream::operator<<(unsigned char i)
+{
+    write(reinterpret_cast<char*>(&i), sizeof(unsigned int));
+
+    return *this;
+}
+
+
+DataStream& DataStream::operator<<(short i)
+{
+    write(reinterpret_cast<char*>(&i), sizeof(short));
+
+    return *this;
+}
+
+
+DataStream& DataStream::operator<<(unsigned short i)
+{
+    write(reinterpret_cast<char*>(&i), sizeof(unsigned short));
+
+    return *this;
+}
+
+
 DataStream& DataStream::operator<<(int i)
 {
-    ByteArray ba(sizeof(int));
-    int* p = reinterpret_cast<int*>(ba.data());
-    *p = i;
+    write(reinterpret_cast<char*>(&i), sizeof(int));
 
-    m_data += ba;
+    return *this;
+}
+
+
+DataStream& DataStream::operator<<(unsigned int i)
+{
+    write(reinterpret_cast<char*>(&i), sizeof(unsigned int));
+
+    return *this;
+}
+
+
+DataStream& DataStream::operator<<(long i)
+{
+    write(reinterpret_cast<char*>(&i), sizeof(long));
+
+    return *this;
+}
+
+
+DataStream& DataStream::operator<<(unsigned long i)
+{
+    write(reinterpret_cast<char*>(&i), sizeof(unsigned long));
+
+    return *this;
+}
+
+
+DataStream& DataStream::operator<<(float f)
+{
+    write(reinterpret_cast<char*>(&f), sizeof(float));
+
+    return *this;
+}
+
+
+DataStream& DataStream::operator<<(double f)
+{
+    write(reinterpret_cast<char*>(&f), sizeof(double));
+
+    return *this;
+}
+
+
+DataStream& DataStream::operator>>(bool& i)
+{
+    read(reinterpret_cast<char*>(&i), sizeof(bool));
+
+    return *this;
+}
+
+
+DataStream& DataStream::operator>>(char& i)
+{
+    read(reinterpret_cast<char*>(&i), sizeof(char));
+
+    return *this;
+}
+
+
+DataStream& DataStream::operator>>(unsigned char& i)
+{
+    read(reinterpret_cast<char*>(&i), sizeof(unsigned char));
+
+    return *this;
+}
+
+
+DataStream& DataStream::operator>>(short& i)
+{
+    read(reinterpret_cast<char*>(&i), sizeof(short));
+
+    return *this;
+}
+
+
+DataStream& DataStream::operator>>(unsigned short& i)
+{
+    read(reinterpret_cast<char*>(&i), sizeof(unsigned short));
 
     return *this;
 }
@@ -36,15 +155,68 @@ DataStream& DataStream::operator<<(int i)
 
 DataStream& DataStream::operator>>(int& i)
 {
+    read(reinterpret_cast<char*>(&i), sizeof(int));
+
+    return *this;
+}
+
+
+DataStream& DataStream::operator>>(unsigned int& i)
+{
+    read(reinterpret_cast<char*>(&i), sizeof(unsigned int));
+
+    return *this;
+}
+
+
+DataStream& DataStream::operator>>(long& i)
+{
+    read(reinterpret_cast<char*>(&i), sizeof(long));
+
+    return *this;
+}
+
+
+DataStream& DataStream::operator>>(unsigned long& i)
+{
+    read(reinterpret_cast<char*>(&i), sizeof(unsigned long));
+
+    return *this;
+}
+
+
+DataStream& DataStream::operator>>(float& f)
+{
+    read(reinterpret_cast<char*>(&f), sizeof(float));
+
+    return *this;
+}
+
+
+DataStream& DataStream::operator>>(double& f)
+{
+    read(reinterpret_cast<char*>(&f), sizeof(double));
+
+    return *this;
+}
+
+
+void DataStream::write(const char* data, int size)
+{
+    m_data.append(data, size);
+}
+
+
+void DataStream::read(char* data, int size)
+{
     // Reference to avoid deep copy
     const ByteArray& ref = m_data;
 
-    const int* p = reinterpret_cast<const int*>(ref.data());
-    i = *p;
+    // Read data
+    const char* dataPtr = ref.data();
+    std::copy(dataPtr, dataPtr + size, data);
 
-    m_data = m_data.middle(sizeof(int));
-
-    return *this;
+    m_data.chopFront(size);
 }
 
 } // namespace Pieces
--- src/pieces/data_stream.h
+++ src/pieces/data_stream.h
@@ -16,11 +16,35 @@ public:
 
     ByteArray data() const;
 
+    DataStream& operator<<(bool i);
+    DataStream& operator<<(char i);
+    DataStream& operator<<(unsigned char i);
+    DataStream& operator<<(short i);
+    DataStream& operator<<(unsigned short i);
     DataStream& operator<<(int i);
+    DataStream& operator<<(unsigned int i);
+    DataStream& operator<<(long i);
+    DataStream& operator<<(unsigned long i);
+    DataStream& operator<<(float f);
+    DataStream& operator<<(double f);
 
+    DataStream& operator>>(bool& i);
+    DataStream& operator>>(char& i);
+    DataStream& operator>>(unsigned char& i);
+    DataStream& operator>>(short& i);
+    DataStream& operator>>(unsigned short& i);
     DataStream& operator>>(int& i);
+    DataStream& operator>>(unsigned int& i);
+    DataStream& operator>>(long& i);
+    DataStream& operator>>(unsigned long& i);
+    DataStream& operator>>(float& f);
+    DataStream& operator>>(double& f);
 
 private:
+
+    void write(const char* data, int size);
+    void read(char* data, int size);
+
     ByteArray m_data;
 };
 
