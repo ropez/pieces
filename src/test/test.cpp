8b109316f32f546b39edd0e770aa40fd480a8c55
Robin Pedersen <robinpeder@gmail.com>
2007-02-21 19:41:25 +0000

    Updated test code.

--- src/test/test.cpp
+++ src/test/test.cpp
@@ -11,11 +11,10 @@ int main()
 {
     using Pieces::ByteArray;
 
-    ByteArray ba("Piece of shit", 14);
-    std::cout << ba << std::endl;
-    std::cin >> ba;
+    const ByteArray ba("Piece of shit", 14);
     std::cout << ba << std::endl;
     std::cout << ba.data() << std::endl;
+
 //
 //     ByteArray b2 = ba;
 //     ByteArray b3 = ba;
@@ -47,11 +46,21 @@ int main()
 //     d >> a >> b >> c;
 //     std::cout << a << " " << b << " " << c << std::endl;
 
+    enum
+    {
+        ROBIN,
+        ANGLE,
+        DOCUMENTATION,
+        SUB_PIECE,
+        SUB_LIST,
+        BYTE_ARRAY
+    };
+
     using Pieces::ValueList;
     using Pieces::PropertyList;
 
     PropertyList t;
-    t.set("robin", 10).set("angle", 2e23).set("documentation", "supercool stuff");
+    t.set(ROBIN, 10).set(ANGLE, 2e23).set(BYTE_ARRAY, ba);
 
     ValueList l;
     l.add('a').add(10).add(2.34).add("joakim simonsson");
@@ -60,22 +69,22 @@ int main()
     std::cout << "A list:\n" << l << std::endl;
 
     std::cout << "Reading from the piece:" << std::endl;
-    std::cout << t.get<int>("robin") << std::endl;
-    std::cout << t.get<double>("angle") << std::endl;
+    std::cout << t.get<int>(ROBIN) << std::endl;
+    std::cout << t.get<double>(ANGLE) << std::endl;
 
-    PropertyList p;
-    p.set("sub-piece", t);
-    p.set("sub-list", l);
-
-    std::cout << "A piece that contains a list and a piece:\n" << p << std::endl;
-
-    std::cout << "Extracted data:" << std::endl;
-    std::cout << p.get<PropertyList>("sub-piece").get<PString>("documentation") << std::endl;
-    std::cout << p.get<ValueList>("sub-list").get<PString>(3) << std::endl;
-
-    ValueList q;
-    q.add(t).add(l);
-
-    std::cout << "Can do the same with a list:\n" << q << std::endl;
+//     PropertyList p;
+//     p.set(SUB_PIECE, t);
+//     p.set(SUB_LIST, l);
+//
+//     std::cout << "A piece that contains a list and a piece:\n" << p << std::endl;
+//
+//     std::cout << "Extracted data:" << std::endl;
+//     std::cout << p.get<PropertyList>(SUB_PIECE).get<PString>(DOCUMENTATION) << std::endl;
+//     std::cout << p.get<ValueList>(SUB_LIST).get<PString>(3) << std::endl;
+//
+//     ValueList q;
+//     q.add(t).add(l);
+//
+//     std::cout << "Can do the same with a list:\n" << q << std::endl;
 }
 
