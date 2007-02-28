dc0a804574e0dac5b2a56691162dddf8359469fc
Thomas Bakken <unxplaind@gmail.com>
2007-02-28 15:09:05 +0000

    Linux implementation of inet_address.cpp. This is untested.

--- Pieces/inet_address.h
+++ Pieces/inet_address.h
@@ -3,6 +3,7 @@
 #define PIECES_INET_ADDRESS_H
 
 #include <string>
+#include <netinet/in.h>
 
 
 namespace Pieces
@@ -15,7 +16,6 @@ public:
     InetAddress();
     ~InetAddress();
 
-
     // String in the form of "12.23.23.2"
     InetAddress(const std::string& addr);
 
@@ -26,10 +26,12 @@ public:
     // String in the form of "12.23.23.2"
     std::string toString() const;
 
+
     // This class also need to be able to return the address as an integral number,
     // or the internal representation might be hidden and the other classes made friend classes.
 
 private:
+	in_addr m_inet_addr;
 
 };
 
--- src/pieces/inet_address.cpp
+++ src/pieces/inet_address.cpp
@@ -1,32 +1,71 @@
 
 #include "Pieces/InetAddress"
-
+#include <arpa/inet.h>
+#include <sys/socket.h>
+#include <netdb.h>
 
 namespace Pieces
 {
 
+/**
+ * Default constructor
+ * Will initiate to 0.0.0.0
+ */
+
 InetAddress::InetAddress()
 {
+	inet_aton("0.0.0.0", &m_inet_addr);
 }
+
+
+/**
+ * Create an InetAddress from a string
+ * in the format "XXX.XXX.XXX.XXX"
+ */
 
 InetAddress::InetAddress(const std::string& addr)
 {
+	//is IP address valid?
+	if( ( inet_aton(addr.c_str(), &m_inet_addr) ) == -1)
+	{
+		//No.. inform user
+	}
 }
+
+
+/**
+ * Resolve a host name and return the address
+ */
 
 InetAddress InetAddress::getHostByName(const std::string& name)
 {
-    InetAddress dummy;
-    return dummy;
+	//Resolve host
+	struct hostent* host = gethostbyname(name.c_str());
+
+	//Create the new InetAddress object with the host IP
+	InetAddress addr(std::string(host->h_addr));
+	
+	return addr;
 }
 
-InetAddress::~InetAddress()
-{
-}
+
+/**
+ * Return a string representation of this address
+ */
 
 std::string InetAddress::toString() const
 {
-    std::string dummy;
-    return dummy;
+	return inet_ntoa(m_inet_addr);
 }
 
-} // namespace Pieces
+
+/**
+ * Destructor
+ */
+
+InetAddress::~InetAddress()
+{
+	
+}
+
+}
