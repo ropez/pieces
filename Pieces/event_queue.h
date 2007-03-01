f3a49e863901858a34f09d82b2ae0fbda5873262
Robin Pedersen <robinpeder@gmail.com>
2007-03-01 13:10:18 +0000

    Doxygen of class EventQueue.

--- Pieces/event_queue.h
+++ Pieces/event_queue.h
@@ -11,15 +11,50 @@ class EventQueuePrivate;
 
 
 /**
- * Thread-safe event queue.
+ * \class EventQueue
+ * \brief Thread-safe event queue.
+ *
+ * This class provides an event queue where events can be pushed and popped
+ * from different threads. The size of the queue is only limited by the amount
+ * of available memory.
+ *
+ * Events are added (produced) with push(), and removed (consumed) with pop().
+ * A thread calling pop() will block if the queue is empty.
+ *
+ * The push() and pop() functions are thread-safe.
+ *
+ * \see Event, EventLoop
+ * \author Robin Pedersen
  */
 class EventQueue
 {
 public:
+
+    /**
+     * Create an empty event queue.
+     */
     EventQueue();
+
+    /**
+     * Destructor.
+     *
+     * Deleting an event-queue that other threads are using is illegal.
+     */
     ~EventQueue();
 
+    /**
+     * Add an event to the end of the queue.
+     *
+     * This will wake up one thread if there are any blocked in pop().
+     */
     void push(const Event& e);
+
+    /**
+     * Pop one event off the queue and return it.
+     *
+     * If the queue is empty, this will block until another thread calls
+     * push().
+     */
     Event pop();
 
 private:
