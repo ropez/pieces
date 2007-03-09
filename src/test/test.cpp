831785e6ca7d41d467ef1d5c2aa90d9e27cb152e
Robin Pedersen <robinpeder@gmail.com>
2007-03-09 08:54:54 +0000

    Test application now use Pieces debug functions.

--- src/test/test.cpp
+++ src/test/test.cpp
@@ -3,53 +3,54 @@
 #include "Pieces/ByteArray"
 #include "Pieces/DataStream"
 #include "Pieces/Vector"
+#include "Pieces/Debug"
 
 #include <iostream>
 #include <sstream>
 
 int main()
 {
-    using Pieces::ByteArray;
-    using Pieces::DataStream;
+    using namespace Pieces;
 
     DataStream ds;
     ds << "Piece of shit";
-    std::cout << ds.data() << std::endl;
+    debug() << ds.data() << " " << ds.data().allocated();
 
     std::string str;
     ds >> str;
-    std::cout << str << std::endl;
+    debug() << str;
 
-//
-//     ByteArray b2 = ba;
-//     ByteArray b3 = ba;
-//     const ByteArray b4 = ba;
-//
-//     b2[0] = 'K';
-//     b2.resize(5);
-//     std::cout << ba.data() << std::endl;
-//     std::cout << b2.data() << std::endl;
-//     std::cout << std::boolalpha << (ba.data() == b3.data()) << std::endl;
-//     std::cout << std::boolalpha << (ba.data() == b4.data()) << std::endl;
-//
-//     std::cout << (b2 + b3).data() << std::endl;
-//
-//     using Pieces::Vector;
-//     Vector<int> vec;
-//     vec.append(6);
-//     vec.append(7);
-//     vec.append(8);
-//
-//     for (Vector<int>::iterator it = vec.begin(); it != vec.end(); ++it)
-//     {
-//         std::cout << "Element: " << *it << std::endl;
-//     }
-//
-//     Pieces::DataStream d;
-//     d << vec[0] << vec[1] << vec[2];
-//     int a, b, c;
-//     d >> a >> b >> c;
-//     std::cout << a << " " << b << " " << c << std::endl;
+
+    ByteArray ba = ds.data();
+    ByteArray b2 = ba;
+    ByteArray b3 = ba;
+    const ByteArray b4 = ba;
+
+    b2[0] = 'K';
+    b2.resize(5);
+    debug() << ba;
+    debug() << b2;
+    debug() << std::boolalpha << (ba.data() == b3.data());
+    debug() << std::boolalpha << (ba.data() == b4.data());
+
+    debug() << (b2 + b3);
+
+    using Pieces::Vector;
+    Vector<int> vec;
+    vec.append(6);
+    vec.append(7);
+    vec.append(8);
+
+    for (Vector<int>::iterator it = vec.begin(); it != vec.end(); ++it)
+    {
+        debug() << "Element: " << *it;
+    }
+
+    Pieces::DataStream d;
+    d << vec[0] << vec[1] << vec[2];
+    int a, b, c;
+    d >> a >> b >> c;
+    debug() << a << " " << b << " " << c;
 
     enum
     {
@@ -72,27 +73,27 @@ int main()
     ValueList l;
     l.add('a').add(10).add(2.34);
 
-    std::cout << "A piece:\n" << t << std::endl;
-    std::cout << "A list:\n" << l << std::endl;
+    debug() << "A piece:\n" << t;
+    debug() << "A list:\n" << l;
 
-    std::cout << "Reading from the piece:" << std::endl;
-    std::cout << t.get<int>(ROBIN) << std::endl;
-    std::cout << t.get<double>(ANGLE) << std::endl;
-    std::cout << t.get<std::string>(STRING) << std::endl;
+    debug() << "Reading from the piece:";
+    debug() << t.get<int>(ROBIN);
+    debug() << t.get<double>(ANGLE);
+    debug() << t.get<std::string>(STRING);
 
     PropertyList p;
     p.set(SUB_PIECE, t);
     p.set(SUB_LIST, l);
 
-    std::cout << "A piece that contains a list and a piece:\n" << p << std::endl;
+    debug() << "A piece that contains a list and a piece:\n" << p;
 
-    std::cout << "Extracted data:" << std::endl;
-    std::cout << p.get<PropertyList>(SUB_PIECE).get<int>(ROBIN) << std::endl;
-    std::cout << p.get<ValueList>(SUB_LIST).get<double>(2) << std::endl;
+    debug() << "Extracted data:";
+    debug() << p.get<PropertyList>(SUB_PIECE).get<int>(ROBIN);
+    debug() << p.get<ValueList>(SUB_LIST).get<double>(2);
 
     ValueList q;
     q.add(t).add(l);
 
-    std::cout << "Can do the same with a list:\n" << q << std::endl;
+    debug() << "Can do the same with a list:\n" << q;
 }
 
