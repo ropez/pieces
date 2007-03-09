778ab2272f4a8469c73888a7ce1065ef9e97221b
Robin Pedersen <robinpeder@gmail.com>
2007-03-09 15:49:13 +0000

    Added another test file with some draft "object" classes.

---
+++ src/test/objects.cpp
@@ -0,0 +1,100 @@
+
+#include "Pieces/SharedData"
+#include "Pieces/SharedDataPointer"
+#include "Pieces/ValueList"
+#include "Pieces/Debug"
+
+
+#include <map>
+
+
+namespace Pieces
+{
+
+class PropertyType
+{
+public:
+    PropertyType();
+
+private:
+    class Data : public SharedData
+    {
+    public:
+        Data();
+
+        int id;
+        std::string description;
+    };
+    SharedDataPointer<Data> d;
+};
+
+
+PropertyType::PropertyType()
+: d(new Data)
+{
+}
+
+
+PropertyType::Data::Data()
+: id(0)
+, description()
+{
+}
+
+
+class ObjectType
+{
+public:
+    ObjectType();
+
+private:
+    class Data : public SharedData
+    {
+    public:
+        std::map<int, PropertyType> properties;
+    };
+
+    SharedDataPointer<Data> d;
+};
+
+
+ObjectType::ObjectType()
+: d(new Data)
+{
+}
+
+
+class ObjectData
+{
+public:
+    ObjectData();
+
+private:
+    class Data : public SharedData
+    {
+    public:
+        ObjectType def;
+        ValueList values;
+    };
+
+    SharedDataPointer<Data> d;
+};
+
+
+ObjectData::ObjectData()
+: d(new Data)
+{
+}
+
+} // namespace Pieces
+
+
+int main()
+{
+    using namespace Pieces;
+
+    debug() << "Hello";
+
+    ObjectData d;
+}
+
