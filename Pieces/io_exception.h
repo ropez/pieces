bd5c2d9bfca0598ab6561f85b335b847eaafb1f9
Thomas Bakken <unxplaind@gmail.com>
2007-03-15 11:18:23 +0000

    Added dummy IOException class

---
+++ Pieces/io_exception.h
@@ -0,0 +1,24 @@
+#ifndef PIECES_IOEXCEPTION_H
+#define PIECES_IOEXCEPTION_H
+
+namespace Pieces
+{
+
+
+/**
+ * \class IOException
+ * \brief Dummy exception class
+ * 
+ * This is nothing but a dummy class for thworing exceptions.
+ * \author Thomas and Tord
+ */
+class IOException
+{
+public:
+
+};
+
+
+} // namespace Pieces
+
+#endif // PIECES_IOEXCEPTION_H
--- src/test/test.cpp
+++ src/test/test.cpp
@@ -4,6 +4,7 @@
 #include "Pieces/DataStream"
 #include "Pieces/Vector"
 #include "Pieces/Debug"
+#include "Pieces/InetAddress"
 
 #include <iostream>
 #include <sstream>
@@ -11,89 +12,23 @@
 int main()
 {
     using namespace Pieces;
+    
+    //make invalid inet address object (0.0.0.0)
+    InetAddress* ia = new InetAddress();
+    
+    //show it
+    std::cout << "Inet address is: " << ia->toString() << std::endl;
 
-    DataStream ds;
-    ds << "Piece of shit";
-    DEBUG << ds.data() << " " << ds.data().allocated();
+    //test constructor giving ip as string argument
+    ia = new InetAddress("192.168.0.1");
 
-    std::string str;
-    ds >> str;
-    DEBUG << str;
+    //show it
+    std::cout << "Inet address is: " << ia->toString() << std::endl;
 
+    //test get host by name 
+    *ia = InetAddress::getHostByName("www.google.com");
 
-    ByteArray ba = ds.data();
-    ByteArray b2 = ba;
-    ByteArray b3 = ba;
-    const ByteArray b4 = ba;
-
-    b2[0] = 'K';
-    b2.resize(5);
-    DEBUG << ba;
-    DEBUG << b2;
-    DEBUG << std::boolalpha << (ba.data() == b3.data());
-    DEBUG << std::boolalpha << (ba.data() == b4.data());
-
-    DEBUG << (b2 + b3);
-
-    using Pieces::Vector;
-    Vector<int> vec;
-    vec.append(6);
-    vec.append(7);
-    vec.append(8);
-
-    for (Vector<int>::iterator it = vec.begin(); it != vec.end(); ++it)
-    {
-        DEBUG << "Element: " << *it;
-    }
-
-    Pieces::DataStream d;
-    d << vec[0] << vec[1] << vec[2];
-    int a, b, c;
-    d >> a >> b >> c;
-    DEBUG << a << " " << b << " " << c;
-
-    enum
-    {
-        ROBIN,
-        ANGLE,
-        DOCUMENTATION,
-        SUB_PIECE,
-        SUB_LIST,
-        BYTE_ARRAY,
-        STRING
-    };
-
-    using Pieces::ValueList;
-    using Pieces::PropertyList;
-
-    PropertyList t;
-    t.set(ROBIN, 10).set(ANGLE, 2e23).set(BYTE_ARRAY, ds.data());
-    t.set(STRING, "Hello world");
-
-    ValueList l;
-    l.add('a').add(10).add(2.34);
-
-    DEBUG << "A piece:\n" << t;
-    DEBUG << "A list:\n" << l;
-
-    DEBUG << "Reading from the piece:";
-    DEBUG << t.get<int>(ROBIN);
-    DEBUG << t.get<double>(ANGLE);
-    DEBUG << t.get<std::string>(STRING);
-
-    PropertyList p;
-    p.set(SUB_PIECE, t);
-    p.set(SUB_LIST, l);
-
-    DEBUG << "A piece that contains a list and a piece:\n" << p;
-
-    DEBUG << "Extracted data:";
-    DEBUG << p.get<PropertyList>(SUB_PIECE).get<int>(ROBIN);
-    DEBUG << p.get<ValueList>(SUB_LIST).get<double>(2);
-
-    ValueList q;
-    q.add(t).add(l);
-
-    DEBUG << "Can do the same with a list:\n" << q;
+    //show it
+    std::cout << "Inet address is: " << ia->toString() << std::endl;    
 }
 
