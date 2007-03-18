3fad61b10091fbab089af9b05ad26198ba55eb76
Robin Pedersen <robinpeder@gmail.com>
2007-03-18 10:25:21 +0000

    Designed a new draft object system.
    All objects implement the abstract interface GameObject.
    Users can create subclasses for their custom objects.
    Users must write functions that save and restore member variables to DataStream.
    Pieces maintain data as a single ByteArray for each object.
    
    This is much better than what we have discussed so far, because users can create their own classes that takes care of both data and functionality in one place. All the user have to do in order to make processes compatible is to use the same set of classes.
    
    What remains is to design a system for how the classes are instantiated in the peers when the host creates objects. I imagine some kind of user-customizable "object factory".

--- src/test/objects.cpp
+++ src/test/objects.cpp
@@ -2,99 +2,220 @@
 #include "Pieces/SharedData"
 #include "Pieces/SharedDataPointer"
 #include "Pieces/ValueList"
+#include "Pieces/PropertyList"
+#include "Pieces/BufferStream"
 #include "Pieces/Debug"
 
 
 #include <map>
-
+#include <cmath>
 
 namespace Pieces
 {
+class GameDataSender;
+class GameDataReceiver;
 
-class PropertyType
+// NOTE: We could also use Universally unique identifiers (e.g. 550e8400-e29b-41d4-a716-446655440000). They take 16 bytes, I have the code we need. (Robin)
+typedef unsigned long objectid_t;
+
+// NOTE: Using 32-bit frame number is enough for about 118 weeks, with 60 frames per second. Enough?
+typedef unsigned long framenum_t;
+
+
+class GameObject
 {
+    friend class GameDataSender;
+    friend class GameDataReceiver;
+
 public:
-    PropertyType();
+    GameObject(objectid_t objectId);
+    virtual ~GameObject();
+
+    objectid_t getObjectId() const;
+    framenum_t getFrameNumber() const;
+
+protected:
+
+    virtual void encode(DataStream& ds) const = 0;
+    virtual void decode(DataStream& ds) = 0;
+
+//     virtual void handle(ObjectEvent* event);
 
 private:
-    class Data : public SharedData
-    {
-    public:
-        Data();
+    // Disable copy operations
+    GameObject(const GameObject&);
+    GameObject& operator=(const GameObject&);
 
-        int id;
-        std::string description;
-    };
-    SharedDataPointer<Data> d;
+    void setFrameNumber(framenum_t number);
+
+    objectid_t m_objectId;
+    framenum_t m_frameNum;
 };
 
 
-PropertyType::PropertyType()
-: d(new Data)
+GameObject::GameObject(objectid_t objectId)
+: m_objectId(objectId)
+, m_frameNum(0)
 {
 }
 
 
-PropertyType::Data::Data()
-: id(0)
-, description()
+GameObject::~GameObject()
 {
 }
 
 
-class ObjectType
+objectid_t GameObject::getObjectId() const
+{
+    return m_objectId;
+}
+
+
+framenum_t GameObject::getFrameNumber() const
+{
+    return m_frameNum;
+}
+
+
+void GameObject::setFrameNumber(framenum_t number)
+{
+    m_frameNum = number;
+}
+
+
+
+// DUMMY simulation of host and peer functionality
+ValueList frames;
+
+class GameDataSender
 {
 public:
-    ObjectType();
 
-private:
-    class Data : public SharedData
+    void sendFrame(GameObject& obj)
     {
-    public:
-        std::map<int, PropertyType> properties;
-    };
+        BufferStream s;
+        obj.encode(s);
 
-    SharedDataPointer<Data> d;
+        frames.addValue(s.data());
+    }
 };
 
-
-ObjectType::ObjectType()
-: d(new Data)
-{
-}
-
-
-class ObjectData
+class GameDataReceiver
 {
 public:
-    ObjectData();
 
-private:
-    class Data : public SharedData
+    void recvFrame(int number, GameObject& obj)
     {
-    public:
-        ObjectType def;
-        ValueList values;
-    };
-
-    SharedDataPointer<Data> d;
+        BufferStream s(frames.getValue(number));
+        obj.decode(s);
+        obj.setFrameNumber(number);
+    }
 };
-
-
-ObjectData::ObjectData()
-: d(new Data)
-{
-}
 
 } // namespace Pieces
 
 
+using namespace Pieces;
+
+
+class MovingBall : public GameObject
+{
+public:
+    MovingBall(objectid_t objectId)
+    : GameObject(objectId)
+    , m_posx(0.0)
+    , m_posy(0.0)
+    , m_diam(10.0)
+    {
+    }
+
+
+    virtual void encode(DataStream& ds) const
+    {
+        ds << m_posx << m_posy << m_diam;
+    }
+
+    virtual void decode(DataStream& ds)
+    {
+        ds >> m_posx >> m_posy >> m_diam;
+    }
+
+
+    double getPosX() const
+    {
+        return m_posx;
+    }
+
+
+    void setPosX(double val)
+    {
+        m_posx = val;
+    }
+
+
+    double getPosY() const
+    {
+        return m_posy;
+    }
+
+
+    void setPosY(double val)
+    {
+        m_posy = val;
+    }
+
+
+    double getDiam() const
+    {
+        return m_diam;
+    }
+
+
+    void setDiam(double val)
+    {
+        m_diam = val;
+    }
+
+private:
+    double m_posx;
+    double m_posy;
+    double m_diam;
+};
+
+
+
+
+
 int main()
 {
-    using namespace Pieces;
+    // Generate data (this is the "host")
+    {
+        GameDataSender sender;
+        MovingBall ball(100);
 
-    DEBUG << "Hello";
+        for (int i = 0; i < 10; ++i)
+        {
+            ball.setPosX(std::sin(i / 10.0));
+            ball.setPosY(std::cos(i / 10.0));
+            ball.setDiam(ball.getDiam() + 1.0);
 
-    ObjectData d;
+            sender.sendFrame(ball);
+        }
+    }
+
+    // Receive data (this is the "peer")
+    {
+        GameDataReceiver receiver;
+        MovingBall ball(100);
+
+        for (int i = 0; i < 10; ++i)
+        {
+            receiver.recvFrame(i, ball);
+            DEBUG << "Moving ball, frame " << ball.getFrameNumber() << ": "
+                << align(40) << "posx = " << ball.getPosX()
+                << align(58) << "posy = " << ball.getPosY()
+                << align(76) << "diam = " << ball.getDiam();
+        }
+    }
 }
 
