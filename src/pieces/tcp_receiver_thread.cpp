1310262d8a14001bd66528399f7068ac87e1fc63
Tord Heimdal <theimdal@gmail.com>
2007-03-19 16:16:21 +0000

    Edited the run function.

--- src/pieces/tcp_receiver_thread.cpp
+++ src/pieces/tcp_receiver_thread.cpp
@@ -1,5 +1,10 @@
 #include "Pieces/TCPReceiverThread"
 #include "Pieces/Debug"
+#include "Pieces/NetworkEvent"
+#include "Pieces/DataStream"
+#include "Pieces/TCPSocket"
+#include "Pieces/EventLoop"
+#include "Pieces/Exception"
 
 
 
@@ -17,7 +22,27 @@ TCPReceiverThread::TCPReceiverThread(TCPSocket* socket, EventLoop* eventLoop)
 
 void TCPReceiverThread::run()
 {
-    DEBUG << "Hei.";
+    try
+    {
+        DataStream ds(m_socket);
+
+        for(;;)
+        {
+            int type;
+            ds >> type;
+
+            ByteArray data;
+            ds >> data;
+
+            std::auto_ptr<Event> e(new NetworkEvent(type));
+            e->setData(data);
+            m_eventLoop->postEvent(e.release());
+        }
+    }
+    catch(const Exception& e)
+    {
+        ERROR << e;
+    }
 }
 
 } // namespace Pieces
