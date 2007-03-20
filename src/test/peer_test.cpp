b601c72ec4577bff3bf9a63fb7af05b86192f26b
Robin Pedersen <robinpeder@gmail.com>
2007-03-20 20:35:48 +0000

    Added some peer functionality, using timer events.

--- src/test/peer_test.cpp
+++ src/test/peer_test.cpp
@@ -3,9 +3,14 @@
 
 #include "Pieces/TCPConnectionManager"
 
+#include "Pieces/Timer"
+#include "Pieces/TimerEvent"
+
 #include "Pieces/InetAddress"
 #include "Pieces/SocketAddress"
 #include "Pieces/AutoPointer"
+
+#include "Pieces/Debug"
 
 #include <cstdlib>
 #include <string>
@@ -13,8 +18,45 @@
 using namespace Pieces;
 
 
+enum MyEvents
+{
+    ID_QUIT_PEER,
+    ID_REPEATING
+};
+
+
 class PeerTest : public Peer
 {
+public:
+    PeerTest()
+    {
+        m_repeating = new Timer(ID_REPEATING, eventLoop());
+        m_repeating->setRepeating(true);
+        m_repeating->start(500);
+    }
+
+protected:
+
+    void handle(TimerEvent* event)
+    {
+        // Handle events
+        DEBUG << "Peer timer-event, id = " << event->getTimerId();
+
+        switch (event->getTimerId())
+        {
+        case ID_REPEATING:
+            // TODO: Send message to host
+            break;
+        case ID_QUIT_PEER:
+            quit();
+            break;
+        default:
+            break;
+        }
+    }
+
+private:
+    AutoPointer<Timer> m_repeating;
 };
 
 
@@ -36,6 +78,10 @@ int main(int argc, char** argv)
 
     SocketAddress address(InetAddress::getHostByName(hostname), port);
     peer->connectionManager()->connectTo(address);
+
+    // This timer stops the Peer
+    Timer tQuitPeer(ID_QUIT_PEER, peer->eventLoop());
+    tQuitPeer.start(10000);
 
     peer->exec();
 }
