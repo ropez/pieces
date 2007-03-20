a5f5257d36171df2bd189430c7e565e1cb609a3f
Robin Pedersen <robinpeder@gmail.com>
2007-03-20 14:07:50 +0000

    Replaced the test example with new code that connects to a "host" and sends network events on port 2222, compatible with the objects test code.

--- src/test/test.cpp
+++ src/test/test.cpp
@@ -1,114 +1,50 @@
-#include "Pieces/PropertyList"
-#include "Pieces/ValueList"
-#include "Pieces/ByteArray"
+#include "Pieces/TCPSocket"
+#include "Pieces/DataStream"
 #include "Pieces/BufferStream"
-#include "Pieces/Vector"
-#include "Pieces/File"
+#include "Pieces/InetAddress"
+#include "Pieces/Exception"
 #include "Pieces/Debug"
 
-#include <iostream>
-#include <sstream>
+#include "OpenThreads/Thread"
+#include "sys/signal.h"
 
-#include <sys/types.h>
-#include <sys/stat.h>
-#include <fcntl.h>
+using namespace Pieces;
 
 
 int main()
 {
-    using namespace Pieces;
+    signal(SIGPIPE, SIG_IGN);
+    OpenThreads::Thread::Init();
 
-    BufferStream ds;
-    ds << "Piece of shit";
-    DEBUG << ds.data() << " " << ds.data().allocated();
-
-    std::string str;
-    ds >> str;
-    DEBUG << str;
-
-
-    ByteArray ba = ds.data();
-    ByteArray b2 = ba;
-    ByteArray b3 = ba;
-    const ByteArray b4 = ba;
-
-    b2[0] = 'K';
-    b2.resize(5);
-    DEBUG << ba;
-    DEBUG << b2;
-    DEBUG << std::boolalpha << (ba.data() == b3.data());
-    DEBUG << std::boolalpha << (ba.data() == b4.data());
-
-    DEBUG << (b2 + b3);
-
-    using Pieces::Vector;
-    Vector<int> vec;
-    vec.append(6);
-    vec.append(7);
-    vec.append(8);
-
-    for (Vector<int>::iterator it = vec.begin(); it != vec.end(); ++it)
+    try
     {
-        DEBUG << "Element: " << *it;
+        TCPSocket sock;
+
+        if (!sock.connect(InetAddress::getHostByName("localhost"), 2222))
+        {
+            ERROR << "Server not found";
+            exit(0);
+        }
+
+        DEBUG << "Connected to " << sock.getPeerAddress();
+
+        DataStream ds(&sock);
+
+        int count = 10;
+        while (count--)
+        {
+            std::stringstream str;
+            str << "Bomb goes off in " << count << " seconds";
+            BufferStream bs;
+            bs << str.str();
+            ds << 666 << bs.data() << flush;
+
+            OpenThreads::Thread::microSleep(1000000);
+        }
     }
-
-    Pieces::BufferStream d;
-    d << vec[0] << vec[1] << vec[2];
-    int a, b, c;
-    d >> a >> b >> c;
-    DEBUG << a << " " << b << " " << c;
-
-    enum
+    catch (const Exception& e)
     {
-        ROBIN,
-        ANGLE,
-        DOCUMENTATION,
-        SUB_PIECE,
-        SUB_LIST,
-        BYTE_ARRAY,
-        STRING
-    };
-
-    using Pieces::ValueList;
-    using Pieces::PropertyList;
-
-    PropertyList t;
-    t.set(ROBIN, 10).set(ANGLE, 2e23).set(BYTE_ARRAY, ds.data());
-    t.set(STRING, "Hello world");
-
-    ValueList l;
-    l.add('a').add(10).add(2.34);
-
-    DEBUG << "A piece:\n" << t;
-    DEBUG << "A list:\n" << l;
-
-    DEBUG << "Reading from the piece:";
-    DEBUG << t.get<int>(ROBIN);
-    DEBUG << t.get<double>(ANGLE);
-    DEBUG << t.get<std::string>(STRING);
-
-    PropertyList p;
-    p.set(SUB_PIECE, t);
-    p.set(SUB_LIST, l);
-
-    DEBUG << "A piece that contains a list and a piece:\n" << p;
-
-    DEBUG << "Extracted data:";
-    DEBUG << p.get<PropertyList>(SUB_PIECE).get<int>(ROBIN);
-    DEBUG << p.get<ValueList>(SUB_LIST).get<double>(2);
-
-    ValueList q;
-    q.add(t).add(l);
-
-    DEBUG << "Can do the same with a list:\n" << q;
-
-    // Write to disk
-    File file("outputfile");
-    if (file.open(File::Write | File::Truncate))
-    {
-        DataStream ds(&file);
-
-        ds << p << flush;
+        ERROR << e;
     }
 }
 
