e998607dbd47100fdf9a58c1c12cbfef80bc1a90
Robin Pedersen <robinpeder@gmail.com>
2010-02-04 22:06:32 +0100

    Run tests individually to get better output

--- src/test/testrunner.cpp
+++ src/test/testrunner.cpp
@@ -1,10 +1,17 @@
 #include <cppunit/ui/text/TestRunner.h>
 #include <cppunit/extensions/TestFactoryRegistry.h>
 
-int main()
+int main(int argc, char** argv)
 {
-    CppUnit::TextUi::TestRunner runner;
+    CppUnit::TextTestRunner runner;
     CppUnit::TestFactoryRegistry& registry = CppUnit::TestFactoryRegistry::getRegistry();
-    runner.addTest(registry.makeTest());
+
+    // Run all, or one specified test
+    CppUnit::Test* test = registry.makeTest();
+    if (argc > 1) {
+        test = test->findTest(argv[1]);
+    }
+    runner.addTest(test);
+
     return runner.run() ? 0 : 1;
 }
