14790bd66ee33261d961160945af2e4e3a972e6b
Robin Pedersen <robinpeder@gmail.com>
2010-02-06 23:23:36 +0100

    Add test case for auto-pointer

---
+++ src/test/test_auto_pointer.cpp
@@ -0,0 +1,89 @@
+#include <cppunit/TestFixture.h>
+#include <cppunit/extensions/HelperMacros.h>
+#include <Pieces/AutoPointer>
+#include <Pieces/global>
+
+using pcs::AutoPointer;
+
+namespace {
+class MockObject
+{
+public:
+    static int count;
+
+    MockObject() {
+        count++;
+    }
+
+    ~MockObject() {
+        count--;
+    }
+
+private:
+    DISABLE_COPY(MockObject);
+};
+int MockObject::count = 0;
+}
+
+class TestAutoPointer : public CppUnit::TestFixture
+{
+    CPPUNIT_TEST_SUITE(TestAutoPointer);
+    CPPUNIT_TEST(testNullPointer);
+    CPPUNIT_TEST(testScopeCleanUp);
+    CPPUNIT_TEST(testManualCleanUp);
+    CPPUNIT_TEST(testOwnerChange);
+    CPPUNIT_TEST(testSelfAssignment);
+    CPPUNIT_TEST_SUITE_END();
+
+public:
+    void setUp() {
+        CPPUNIT_ASSERT_EQUAL(0, MockObject::count);
+    }
+
+    void tearDown() {
+        CPPUNIT_ASSERT_EQUAL(0, MockObject::count);
+    }
+
+    void testNullPointer() {
+        AutoPointer<MockObject> p;
+        CPPUNIT_ASSERT(p.isNull());
+        CPPUNIT_ASSERT(!p.isValid());
+        CPPUNIT_ASSERT(!p);
+        CPPUNIT_ASSERT(p.get() == 0);
+    }
+
+    void testScopeCleanUp() {
+        AutoPointer<MockObject> p(new MockObject);
+        CPPUNIT_ASSERT_EQUAL(1, MockObject::count);
+    }
+
+    void testManualCleanUp() {
+        AutoPointer<MockObject> p;
+        CPPUNIT_ASSERT_EQUAL(0, MockObject::count);
+        p = new MockObject;
+        CPPUNIT_ASSERT_EQUAL(1, MockObject::count);
+        p = 0;
+        CPPUNIT_ASSERT_EQUAL(0, MockObject::count);
+    }
+
+    void testOwnerChange() {
+        AutoPointer<MockObject> p(new MockObject);
+        CPPUNIT_ASSERT_EQUAL(1, MockObject::count);
+        AutoPointer<MockObject> q = p;
+        CPPUNIT_ASSERT_EQUAL(1, MockObject::count);
+        CPPUNIT_ASSERT(q.isValid());
+        CPPUNIT_ASSERT(p.isNull());
+        p = q;
+        CPPUNIT_ASSERT_EQUAL(1, MockObject::count);
+        CPPUNIT_ASSERT(p.isValid());
+        CPPUNIT_ASSERT(q.isNull());
+    }
+
+    void testSelfAssignment() {
+        AutoPointer<MockObject> p(new MockObject);
+        p = p;
+        CPPUNIT_ASSERT_EQUAL(1, MockObject::count);
+        CPPUNIT_ASSERT(p.isValid());
+    }
+};
+CPPUNIT_TEST_SUITE_REGISTRATION(TestAutoPointer);
