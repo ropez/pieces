68c89c675e41c15503a0a5a1211d708cc5ece400
Robin Pedersen <robinpeder@gmail.com>
2010-02-07 10:21:59 +0100

    Add test case for implicit sharing pointer

--- src/test/test_smart_pointers.cpp
+++ src/test/test_smart_pointers.cpp
@@ -3,10 +3,12 @@
 #include <Pieces/SharedData>
 #include <Pieces/AutoPointer>
 #include <Pieces/ReferencePointer>
+#include <Pieces/SharedDataPointer>
 #include <Pieces/global>
 
 using pcs::AutoPointer;
 using pcs::ReferencePointer;
+using pcs::SharedDataPointer;
 
 namespace {
 class MockObject : public pcs::SharedData
@@ -26,6 +28,15 @@ private:
     DISABLE_COPY(MockObject);
 };
 int MockObject::count = 0;
+
+class MockData : public MockObject
+{
+public:
+    MockData() : MockObject() {}
+    MockData(const MockData& other) : MockObject() {}
+
+    void foo() {}
+};
 }
 
 class TestSharedData : public CppUnit::TestFixture
@@ -195,4 +206,90 @@ public:
 };
 CPPUNIT_TEST_SUITE_REGISTRATION(TestReferencePointer);
 
+class TestSharedDataPointer : public CppUnit::TestFixture
+{
+    CPPUNIT_TEST_SUITE(TestSharedDataPointer);
+    CPPUNIT_TEST(testNullPointer);
+    CPPUNIT_TEST(testScopeCleanUp);
+    CPPUNIT_TEST(testManualCleanUp);
+    CPPUNIT_TEST(testMultipleReferences);
+    CPPUNIT_TEST(testAutomaticDetach);
+    CPPUNIT_TEST(testSelfAssignment);
+    CPPUNIT_TEST_SUITE_END();
+
+public:
+    void setUp() {
+        MockObject::count = 0;
+    }
+
+    void tearDown() {
+        CPPUNIT_ASSERT_EQUAL(0, MockObject::count);
+    }
+
+    void testNullPointer() {
+        SharedDataPointer<MockData> p;
+        CPPUNIT_ASSERT(p.const_data() == 0);
+    }
+
+    void testScopeCleanUp() {
+        SharedDataPointer<MockData> p(new MockData);
+        CPPUNIT_ASSERT_EQUAL(1, MockObject::count);
+    }
+
+    void testManualCleanUp() {
+        SharedDataPointer<MockData> p;
+        CPPUNIT_ASSERT_EQUAL(0, MockObject::count);
+        p = new MockData;
+        CPPUNIT_ASSERT_EQUAL(1, MockObject::count);
+        p = new MockData;
+        CPPUNIT_ASSERT_EQUAL(1, MockObject::count);
+        p = 0;
+        CPPUNIT_ASSERT_EQUAL(0, MockObject::count);
+    }
+
+    void testMultipleReferences() {
+        SharedDataPointer<MockData> p(new MockData);
+        CPPUNIT_ASSERT_EQUAL(1, MockObject::count);
+        SharedDataPointer<MockData> q(new MockData);
+        CPPUNIT_ASSERT_EQUAL(2, MockObject::count);
+        q = p;
+        CPPUNIT_ASSERT_EQUAL(1, MockObject::count);
+        CPPUNIT_ASSERT(q.const_data() != 0);
+        CPPUNIT_ASSERT(p.const_data() != 0);
+        CPPUNIT_ASSERT_EQUAL(q.const_data(), p.const_data());
+        CPPUNIT_ASSERT(p.const_data()->shared());
+        p = q;
+        CPPUNIT_ASSERT_EQUAL(1, MockObject::count);
+        CPPUNIT_ASSERT(q.const_data() != 0);
+        CPPUNIT_ASSERT(p.const_data() != 0);
+        CPPUNIT_ASSERT_EQUAL(q.const_data(), p.const_data());
+    }
+
+    void testAutomaticDetach() {
+        SharedDataPointer<MockData> p(new MockData);
+        CPPUNIT_ASSERT_EQUAL(1, MockObject::count);
+        SharedDataPointer<MockData> q(new MockData);
+        CPPUNIT_ASSERT_EQUAL(2, MockObject::count);
+        q = p;
+        CPPUNIT_ASSERT_EQUAL(1, MockObject::count);
+        CPPUNIT_ASSERT_EQUAL(q.const_data(), p.const_data());
+        CPPUNIT_ASSERT(p.const_data()->shared());
+
+        p->foo(); // access data directly, causing detach
+
+        CPPUNIT_ASSERT_EQUAL(2, MockObject::count);
+        CPPUNIT_ASSERT(q.const_data() != p.const_data());
+        CPPUNIT_ASSERT(!p.const_data()->shared());
+        CPPUNIT_ASSERT(!q.const_data()->shared());
+    }
+
+    void testSelfAssignment() {
+        SharedDataPointer<MockData> p(new MockData);
+        p = p;
+        CPPUNIT_ASSERT_EQUAL(1, MockObject::count);
+        CPPUNIT_ASSERT(p.const_data() != 0);
+        CPPUNIT_ASSERT(!p.const_data()->shared());
+    }
+};
+CPPUNIT_TEST_SUITE_REGISTRATION(TestSharedDataPointer);
 
