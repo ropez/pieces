ffdca98674c1e7bfd545748707cc0160ba1f8283
Robin Pedersen <robinpeder@gmail.com>
2010-02-27 23:45:44 +0100

    Add tests for inet-address conversion and comparison

--- src/test/test_inet_address.cpp
+++ src/test/test_inet_address.cpp
@@ -11,6 +11,8 @@ class TestInetAddress : public CppUnit::TestFixture
     CPPUNIT_TEST(testParseAddress);
     CPPUNIT_TEST(testParseError);
     CPPUNIT_TEST(testFromBinary);
+    CPPUNIT_TEST(testCopy);
+    CPPUNIT_TEST(testCompare);
     CPPUNIT_TEST_SUITE_END();
 
     unsigned int ip4(unsigned int b1, unsigned int b2, unsigned int b3, unsigned int b4) {
@@ -34,7 +36,7 @@ public:
         CPPUNIT_ASSERT(!addr2.isNull());
         CPPUNIT_ASSERT_EQUAL(std::string("1.0.0.127"), addr2.toString());
         CPPUNIT_ASSERT_EQUAL(ip4(1, 0, 0, 127), addr2.toInt32());
-        InetAddress addr3("255.255.255.255");
+        InetAddress addr3 = std::string("255.255.255.255");
         CPPUNIT_ASSERT(!addr3.isNull());
         CPPUNIT_ASSERT_EQUAL(std::string("255.255.255.255"), addr3.toString());
         CPPUNIT_ASSERT_EQUAL(ip4(255, 255, 255, 255), addr3.toInt32());
@@ -54,6 +56,31 @@ public:
         CPPUNIT_ASSERT_EQUAL(std::string("1.0.0.127"), addr2.toString());
         InetAddress addr3(ip4(255, 255, 255, 255));
         CPPUNIT_ASSERT_EQUAL(std::string("255.255.255.255"), addr3.toString());
+    }
+
+    void testCopy() {
+        InetAddress addr1("1.2.3.4");
+        InetAddress addr2 = addr1;
+        CPPUNIT_ASSERT_EQUAL(std::string("1.2.3.4"), addr2.toString());
+        InetAddress addr3;
+        CPPUNIT_ASSERT(addr3.isNull());
+        addr3 = addr1;
+        CPPUNIT_ASSERT_EQUAL(std::string("1.2.3.4"), addr3.toString());
+    }
+
+    void testCompare() {
+        InetAddress addrNull;
+        InetAddress addr1a("1.2.3.4");
+        InetAddress addr1b(ip4(1, 2, 3, 4));
+        InetAddress addr2a("255.255.255.255");
+        InetAddress addr2b(ip4(255, 255, 255, 255));
+
+        CPPUNIT_ASSERT(addrNull == InetAddress());
+        CPPUNIT_ASSERT(addrNull != addr1a);
+        CPPUNIT_ASSERT(addrNull != addr2b);
+        CPPUNIT_ASSERT(addr1a != addr2a);
+        CPPUNIT_ASSERT(addr1a == addr1b);
+        CPPUNIT_ASSERT(addr2a == addr2b);
     }
 };
 CPPUNIT_TEST_SUITE_REGISTRATION(TestInetAddress);
